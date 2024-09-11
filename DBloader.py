ca39f9e4dc55d787f75f5901bc4839067a999544
karolbala <157180826+karolbala@users.noreply.github.com>
2024-09-11 15:32:55 +0200

    Update DBloader.py

--- DBloader.py
+++ DBloader.py
@@ -3,18 +3,40 @@ import psycopg2
 from config import load_config
 
 def connect(config):
+    """
+    Connects to a PostgreSQL database using the provided configuration.
+
+    Args:
+        config (dict): A dictionary containing the database configuration parameters.
+
+    Returns:
+        conn: A connection object to the PostgreSQL database.
+    """
     try:
         with psycopg2.connect(**config) as conn:
             print('Connected to the PostgreSQL server.')
             return conn
     except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
+        print(f"An error occurred: {error}")
 
-conn=connect(load_config())
+conn = connect(load_config())
 
-def table_loader(gdf,name, engine='postgresql',user='igp419798', password='igp563669', host='ak.geod.agh.edu.pl',port='5432',db='igp419798'):
-    db_connection_url = f"{engine}://{user}:{password}@{host}:{port}/{db}"
+def table_loader(gdf, table_name, engine, user, password, host, port, database):
+    """
+    Loads a GeoDataFrame into a PostGIS table in the PostgreSQL database.
+
+    Args:
+        gdf (GeoDataFrame): The GeoDataFrame containing spatial data to be loaded.
+        table_name (str): The name of the table to load the data into.
+        engine (str): The database engine (e.g., 'postgresql').
+        user (str): The database username.
+        password (str): The database password.
+        host (str): The database host address.
+        port (int): The database port number.
+        database (str): The name of the database.
+
+    """
+    db_connection_url = f"{engine}://{user}:{password}@{host}:{port}/{database}"
     engine = create_engine(db_connection_url)
-    gdf.to_postgis(name, con=engine, if_exists= "replace")
-
-
+    gdf.to_postgis(table_name, con=engine, if_exists="replace")
+    print(f"Data loaded into table {table_name} in the {database} database.")
