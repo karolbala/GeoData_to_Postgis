496c9ccb7b02de939267297d52409e9c6de1a459
karolbala <157180826+karolbala@users.noreply.github.com>
2024-09-11 15:32:58 +0200

    Update SHP.py

--- SHP.py
+++ SHP.py
@@ -1,22 +1,18 @@
 from sqlalchemy import create_engine
 import geopandas as gpd
 
-
-
 def import_shapefile_to_postgis(table_name, shapefile, user, password, host, port, database):
-
     """
-    Importuje dane z pliku shapefile do bazy danych PostGIS
+    Imports data from a shapefile into a PostGIS database.
 
     Args:
-        table_name (str): Nazwa tabeli, do której będą importowane dane.
-        shapefile (str): Ścieżka do pliku shapefile.
-        user (str): Nazwa użytkownika bazy danych.
-        password (str): Hasło użytkownika bazy danych.
-        host (str): Adres hosta bazy danych.
-        port (str): Numer portu serwera.
-        database (str): Nazwa bazy danych.
-
+        table_name (str): Name of the table to import the data into.
+        shapefile (str): Path to the shapefile.
+        user (str): Database username.
+        password (str): Database user password.
+        host (str): Database host address.
+        port (str): Server port number.
+        database (str): Name of the database.
     """
 
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
@@ -24,8 +20,8 @@ def import_shapefile_to_postgis(table_name, shapefile, user, password, host, por
         gdf = gpd.read_file(shapefile)
         gdf = gdf.to_crs(2180)
         gdf.to_postgis(table_name, engine, if_exists='replace', index=False)
-        print(f"Wczytano dane z pliku {shapefile} do tabeli {table_name} w bazie danych {database}")
+        print(f"Data from {shapefile} has been successfully loaded into the table {table_name} in the {database} database")
     except FileNotFoundError as e:
-        print(f"Błąd: {e}. Upewnij się, że plik shapefile o podanej nazwie istnieje.")
+        print(f"Error: {e}. Please make sure the shapefile exists.")
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        print(f"An error occurred: {e}")
