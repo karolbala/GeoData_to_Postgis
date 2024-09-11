8e32d7a55469c5d4ad0f7ced31ccf7098d34ba03
karolbala <157180826+karolbala@users.noreply.github.com>
2024-09-11 15:36:50 +0200

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -5,32 +5,29 @@ from sqlalchemy import create_engine
 import geopandas as gpd
 import shapely.wkb as sh
 
-
-
 def import_sql_to_postgis(table_name, file, user, password, host, port, database):
     """
-       Importuje dane z pliku SQL do tabeli PostGIS w bazie danych PostgreSQL.
+    Imports data from an SQL file into a PostGIS table in a PostgreSQL database.
 
-       Args:
-           table_name (str): Nazwa tabeli, do której będą importowane dane.
-           file (str): Ścieżka do pliku SQL zawierającego dane.
-           user (str): Nazwa użytkownika bazy danych.
-           password (str): Hasło użytkownika bazy danych.
-           host (str): Adres hosta bazy danych.
-           port (str): Numer portu serwera.
-           database (str): Nazwa bazy danych.
+    Args:
+        table_name (str): Name of the table to import data into.
+        file (str): Path to the SQL file containing the data.
+        user (str): Database username.
+        password (str): Database user password.
+        host (str): Database host address.
+        port (str): Server port number.
+        database (str): Name of the database.
 
-       Raises:
-           KeyError: Występuje, gdy kolumna 'wkb_geometry' nie istnieje w pliku SQL.
-           FileNotFoundError: Występuje, gdy plik SQL nie zostanie odnaleziony.
-           Exception: Inne występujące błędy.
+    Raises:
+        KeyError: Raised if the 'wkb_geometry' column is missing in the SQL file.
+        FileNotFoundError: Raised if the SQL file cannot be found.
+        Exception: Any other errors that may occur.
+    """
 
-       """
-
-    #Tworzenie silnika
+    # Create the engine
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
 
-    # Funkcja pozyskująca wnętrze INSERTÓW z pliku SQL
+    # Function to extract the data inside INSERT statements from the SQL file
     def display_insert_lines(file_path):
         data_list = []
         try:
@@ -46,7 +43,7 @@ def import_sql_to_postgis(table_name, file, user, password, host, port, database
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    # Funkcja pozyskująca nazwy kolumn
+    # Function to extract column names
     def get_columns(file_path):
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
@@ -64,27 +61,28 @@ def import_sql_to_postgis(table_name, file, user, password, host, port, database
     columns = get_columns(file)
 
     try:
-        # Tworzenie płaskiej ramki danych
+        # Create a flat DataFrame
         df = pd.DataFrame(data=data, columns=columns)
 
-        df['wkb_geometry '] = df['wkb_geometry '].str.strip().str.replace("'", "")  # Usunięcie cudzysłowów i spacji
+        df['wkb_geometry'] = df['wkb_geometry'].str.strip().str.replace("'", "")  # Remove quotes and spaces
 
-        # Tworzenie kolumny geometrii poprzez mapowanie kolumny z danymi w formacie WKB funkcją z biblioteki Shapely
-        df['geometry'] = df['wkb_geometry '].map(sh.loads)
+        # Create a geometry column by mapping the WKB data using Shapely
+        df['geometry'] = df['wkb_geometry'].map(sh.loads)
 
-        # Przekształcenie geometrii do 1-wymiarowego wektora
+        # Convert geometry into a 1D vector
         geometry = np.array(df['geometry'])
         geometry = geometry.flatten()
 
-        # Zamiana ramki na GeoDataFrame
+        # Convert the DataFrame to a GeoDataFrame
         gdf = gpd.GeoDataFrame(df, crs="EPSG:2180", geometry=geometry)
-        # Zmiana układu współrzędnych
+        # Transform the coordinate system
         gdf = gdf.to_crs(2180)
 
-        gdf.to_postgis(table_name, engine, if_exists='replace',index=False)  # Aktualizacja zapisu nazwy kolumny geometrii
-        print(f"Wczytano dane z pliku SQL do tabeli {table_name} w bazie danych {database}")
+        # Load data into PostGIS table
+        gdf.to_postgis(table_name, engine, if_exists='replace', index=False)
+        print(f"Data from the SQL file has been successfully loaded into the {table_name} table in the {database} database.")
 
     except KeyError as e:
-        print(f"Błąd: {e}. Upewnij się, że kolumna 'wkb_geometry' istnieje w pliku SQL.")
+        print(f"Error: {e}. Please ensure the 'wkb_geometry' column exists in the SQL file.")
     except Exception as e:
-        print(f"Wystąpił błąd: {e}")
+        print(f"An error occurred: {e}")
